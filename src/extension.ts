0f104ced8a0cac8a7c6b08250278e8218851b5f7
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2023-04-01 14:39:03 -0300

    feat: welcome button

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,16 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "log-whisperer" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "log-whisperer" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "log-whisperer.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World from log-whisperer!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('log-whisperer.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from log-whisperer!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
