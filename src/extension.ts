44ac6d77ecea4af440adb76176afa2878c2e64d4
Carlos Montecinos <carlos.w.montecinos@gmail.com>
2023-04-01 14:50:36 -0300

    feat: get the path of the selected folder

--- src/extension.ts
+++ src/extension.ts
@@ -3,14 +3,33 @@ import * as vscode from "vscode";
 export function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "log-whisperer" is now active!');
 
-  let disposable = vscode.commands.registerCommand(
-    "log-whisperer.helloWorld",
+  let selectLogFolder = vscode.commands.registerCommand(
+    "log-whisperer.selectLogFolder",
     () => {
-      vscode.window.showInformationMessage("Hello World from log-whisperer!");
+      const folderPath = vscode.window.showOpenDialog({
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: true,
+        openLabel: "Select log folder",
+      });
+
+      // get the path of the selected folder
+      folderPath.then((value) => {
+        if (value) {
+          const folderPath = value[0].fsPath;
+          vscode.window.showInformationMessage(folderPath);
+          console.log(folderPath);
+        }
+      });
+
+      // save the path in the workspace settings
+      vscode.workspace
+        .getConfiguration("log-whisperer")
+        .update("logFolder", folderPath, vscode.ConfigurationTarget.Workspace);
     }
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(selectLogFolder);
 }
 
 export function deactivate() {}
