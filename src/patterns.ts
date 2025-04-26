ecb4d19c19ba75f80ddd876491df663b05afb6c1
Carlos <carlos@pythoncheatsheet.org>
2025-04-26 09:43:15 -0400

    Remove log-whisperer extension files and resources, including icon, main logic, patterns, test files, and configuration.

--- src/patterns.ts
+++
@@ -1,9 +0,0 @@
-const startWithDatePattern = new RegExp(
-  "^(\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2},\\d{3}.*$)",
-  "gm"
-);
-const logLevelPattern = new RegExp(/(INFO|ERROR|DEBUG|WARNING)/);
-
-const datePattern = new RegExp(/(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3})/);
-
-export { startWithDatePattern, logLevelPattern, datePattern };
